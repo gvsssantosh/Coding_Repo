596e209920a5666289588e417d4fe1121c8ec415
gvsssantosh <95643944+gvsssantosh@users.noreply.github.com>
2026-05-27 17:22:14 +0530

    Create 283-MovingZeros.py

---
+++ 283-MovingZeros.py
@@ -0,0 +1,19 @@
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        i=0
+        j=i+1
+        while(i<j and j<len(nums)):
+            
+            if(nums[i]==0):
+                if(nums[j]!=0):
+                    nums[i],nums[j]=nums[j],nums[i]
+                    i=i+1
+                # else:
+                #     j=j+1
+
+            else:
+                i=i+1
+            j=j+1
